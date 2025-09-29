7fa20baac965bac18cc3404316953385bdac361a
ajhethcote <andreajhethcote@yahoo.com>
2025-09-28 21:30:49 -0400

    Add files via upload

---
+++ endsWith7kyu.py
@@ -0,0 +1,17 @@
+#Title of the kata
+    #String ends with?
+
+#Link to the kata on Codewars:
+    #https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/python
+
+#Your solution code:
+    #As seen below.
+
+#A short explanation (3–4 sentences) of how your solution works.
+    #Admittedly, I had to look at the solution, but doing so taught me that 'endswith' is a function!!! 
+    #I'm beginning to believe there is a function for everything...
+    #I made a very long overly complicated apparatus before kinda giving up and looking at the solution. But I learned!!! 
+    #I am considering this a success.
+
+#uses the awesome 'endswith' function to check if 'text' ends with 'ending'. Love this function. Would give my firstborn child to this function.
+return text.endswith(ending)
